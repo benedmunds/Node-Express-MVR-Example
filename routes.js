092a5bffcb933dfc0f0abbea7dcefa73a2043cce
Ben Edmunds <ben.edmunds@gmail.com>
2012-04-27 11:51:42 -0400

    Added view example route and view

--- routes.js
+++ routes.js
@@ -8,15 +8,37 @@ module.exports = function(app, models){
     if (app.requireAuth === true && req.loggedIn === false)
       res.redirect('/auth/twitter');
 
-    //get all the rides
+    //get all the examples
     models.examples.find({}, function(err, docs){
       
       //render the index page
       res.render('index.jade', {
           locals: {
             title: 'Example',
-            search_placeholder: 'Search',
             examples: docs
+          }
+      });
+
+    });
+  });
+
+
+  /**
+   *  View
+   */
+  app.get('/view/:id', function(req, res){
+
+    if (app.requireAuth === true && req.loggedIn === false)
+      res.redirect('/auth/twitter');
+
+    //get the example
+    models.examples.findById(req.params.id, function(err, doc){
+      
+      //render the view page
+      res.render('view.jade', {
+          locals: {
+            title: 'Example',
+            example: doc
           }
       });
 
